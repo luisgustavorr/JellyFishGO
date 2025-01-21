3545c1e36e80cd60c7ce688bb793435bc5dc1192
= <=>
2025-01-21 18:58:07 -0300

    updating to use pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+exports.apps = [
+    {
+        name: "try-go",
+        script: "jellyFish",
+        interpreter: "none", // instruct PM2 that the "script" doesn't rely on an interpreter
+    }
+];
