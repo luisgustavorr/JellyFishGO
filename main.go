a75e8a42ed4f35c74fea01d8d95dc16b0a671eec
= <=>
2025-01-21 18:40:02 -0300

    setting port

--- main.go
+++ main.go
@@ -892,7 +892,7 @@ func main() {
 			}
 		}
 	})
-	r.Run(":3000") // Escutando na porta 8080
+	r.Run(":3030") // Escutando na porta 8080
 }
 func desconctarCliente(clientId string) bool {
 	fmt.Println("Desconectando " + clientId + " ...")
