9ae0cfc65d09327a6fc01b797d00b20cfff7712b
= <=>
2025-03-10 18:24:41 -0300

    update log on mensagem repetida

--- main.go
+++ main.go
@@ -554,7 +554,7 @@ func handleMessage(fullInfoMessage *events.Message, clientId string, client *wha
 			var MessageID []types.MessageID = []types.MessageID{id_message}
 			client.MarkRead(MessageID, time.Now(), JID, JID, types.ReceiptTypeRead)
 		}
-		fmt.Println("MENSAGEM COM ID JÁ ENVIADO")
+		fmt.Println("!--------------------->MENSAGEM COM ID JÁ ENVIADO<---------------------!")
 		return false // Ignora mensagem já processada
 	}
 	params := &whatsmeow.GetProfilePictureParams{}
