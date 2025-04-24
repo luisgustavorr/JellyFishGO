32f7e8dc8ad179e846ef3d4c2f55be41aafa2d9d
= <=>
2025-04-24 14:22:52 -0300

    update menor q 13

--- main.go
+++ main.go
@@ -1209,7 +1209,7 @@ func processarGrupoMensagens(sendInfoMain sendMessageInfo) {
 			re := regexp.MustCompile("[0-9]+")
 			numberWithOnlyNumbers := strings.Join(re.FindAllString(item["number"].(string), -1), "")
 			if len(numberWithOnlyNumbers) > 2 {
-				if numberWithOnlyNumbers[:2] != "55" && len(numberWithOnlyNumbers) < 13 {
+				if numberWithOnlyNumbers[:2] != "55" && len(numberWithOnlyNumbers) <= 13 {
 					numberWithOnlyNumbers = "55" + numberWithOnlyNumbers
 				}
 			} else {
