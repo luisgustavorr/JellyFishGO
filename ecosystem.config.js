c476abe739d1e2438d0edb7ae4ecf9dceb028d3f
luisgustavorr <luisgustavo20061@gmail.com>
2025-06-26 16:34:53 -0300

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 exports.apps = [
     {
         name: "jellyFishGO",
-        script: "jellyFish.exe",
-        interpreter: "none", // instruct PM2 that the "script" doesn't rely on an interpreter
+        script: "jellyFish",
+        interpreter: "none", // instruct PM2 that the "script" doesn't rely on >
+        max_memory_restart: '700M'
     }
 ];
