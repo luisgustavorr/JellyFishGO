b809a4ff4e0cc85d7b78c340ac7500f29199da29
luisgustavorr <luisgustavo20061@gmail.com>
2025-07-02 09:36:15 -0300

    update teste

--- main.go
+++ main.go
@@ -2306,6 +2306,7 @@ func converterParaOgg(inputPath string) (string, error) {
 		"-i", inputPath, // entrada
 		"-c:a", "libopus", // codec de voz
 		"-b:a", "16k", // taxa de bits baixa (voz)
+		"-f ogg", //  força o container certo
 		outputPath,
 	)
 
